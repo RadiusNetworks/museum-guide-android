e74d8d9d985aa4f83d5371f1a73e7c5388ac59e2
David G. Young <dyoung@Davids-MacBook-Pro-2.local>
2014-03-23 16:30:11 -0400

    add license header

--- app/src/main/java/com/radiusnetworks/museumguide/ScreenFragment.java
+++ app/src/main/java/com/radiusnetworks/museumguide/ScreenFragment.java
@@ -1,3 +1,16 @@
+/**
+ *
+ * Copyright (c) 2013,2014 RadiusNetworks. All rights reserved.
+ * http://www.radiusnetworks.com
+ *
+ * @author David G. Young
+ *
+ * Licensed to the Attribution Assurance License (AAL)
+ * (adapted from the original BSD license) See the LICENSE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ */
 package com.radiusnetworks.museumguide;
 
 import android.os.Bundle;
